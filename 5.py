6be8bef914a18ba01d2b560721fab5ba3de9a74c
Kuga09 <glal64@tutamail.com>
2024-12-18 10:55:19 +0300

    5

--- 5.py
+++ 5.py
@@ -1,9 +1,15 @@
-def f(a,b):
+def GCD(a,b):
     if b == 0:
         print(a)
     else:
         c=a
         a=b
         b=c%b
-        return f(a,b)
-d=f(int(input()),int(input()))
+        return GCD(a,b)
+inpt=input()
+inpt=inpt.split()
+a=int(inpt[0])
+b=int(inpt[1])
+print(a)
+print(b)
+d=GCD(a,b)
